ef1be3f2bf0c93aeb2c0a6b755a3b702cfdb061a
Michael Cauley <jmcauley04@gmail.com>
2021-12-31 14:33:11 -0500

    Added codebox example

--- Blazyor/Extensions/EnumExtensions.cs
+++ Blazyor/Extensions/EnumExtensions.cs
@@ -21,25 +21,5 @@ namespace Blazyor.Extensions
             }
             return GenericEnum.ToString();
         }
-
-        public static string GetDisplayName(this Enum enumValue)
-        {
-            if (enumValue == null)
-                return "";
-
-            string displayName;
-
-            displayName = enumValue.GetType()
-                .GetMember(enumValue.ToString())
-                .FirstOrDefault()?
-                .GetCustomAttribute<DisplayAttribute>()?
-                .GetName();
-
-            if (String.IsNullOrEmpty(displayName))
-            {
-                displayName = enumValue.ToString();
-            }
-            return displayName;
-        }
     }
 }
