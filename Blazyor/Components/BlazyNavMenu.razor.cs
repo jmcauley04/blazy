839b275aad172de8383c306608badec9a2582cf1
Michael Cauley <jmcauley04@gmail.com>
2022-01-01 07:33:16 -0500

    Nav Menu flex with hover done

---
+++ Blazyor/Components/BlazyNavMenu.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+
+namespace Blazyor.Components
+{
+    public partial class BlazyNavMenu
+    {
+        [Parameter]
+        public IEnumerable<BlazyNavLink> Links { get; set; }
+
+        public class BlazyNavLink
+        {
+            public string Text { get; set; } = string.Empty;
+
+            public string? Href { get; set; }
+
+            public IEnumerable<BlazyNavLink>? Sublinks { get; set; }
+
+            public NavLinkMatch LinkMatch { get; set; } = NavLinkMatch.All;
+        }
+    }
+}
