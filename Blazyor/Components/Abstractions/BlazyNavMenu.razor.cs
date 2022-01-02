d1f288f2f57946b740ffa09223769093dfb14d00
Michael Cauley <jmcauley04@gmail.com>
2022-01-02 13:18:55 -0500

    Top nav and side nav and layouts implemented

---
+++ Blazyor/Components/Abstractions/BlazyNavMenu.razor.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+
+namespace Blazyor.Components.Abstractions
+{
+    public partial class BlazyNavMenu
+    {
+        [Parameter]
+        public IEnumerable<BlazyNavLink> Links { get; set; } = new List<BlazyNavLink>();
+
+        public class BlazyNavLink
+        {
+            public string Text { get; set; } = string.Empty;
+
+            public string? Href { get; set; }
+
+            public Action? OnClick { get; set; }
+
+            public IEnumerable<BlazyNavLink>? Sublinks { get; set; }
+
+            public NavLinkMatch LinkMatch { get; set; } = NavLinkMatch.All;
+
+            public string SublinksStyle { get; set; } = string.Empty;
+        }
+
+        void SetSublinkStyle(BlazyNavLink navlink)
+        {
+            if (string.IsNullOrEmpty(navlink.SublinksStyle))
+                navlink.SublinksStyle = $"max-height: {52 * navlink.Sublinks!.Count()}px;";
+            else
+                navlink.SublinksStyle = string.Empty;
+        }
+    }
+}
---
+++ Blazyor/Usings.cs
@@ -0,0 +1,3 @@
+﻿global using Blazyor.Extensions;
+global using Blazyor.Fragments;
+global using Blazyor.Components;
