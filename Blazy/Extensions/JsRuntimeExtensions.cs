7ed10a1c99e129f8a5485e41c7aaa72eca70ba17
Michael Cauley <jmcauley04@gmail.com>
2022-01-03 15:56:06 -0500

    Added an Icons page

---
+++ Blazy/Extensions/JsRuntimeExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.JSInterop;
+using Microsoft.JSInterop.Implementation;
+
+namespace Blazy.Extensions
+{
+    internal static class JsRuntimeExtensions
+    {
+        internal static async Task<IJSObjectReference> Import(
+            this IJSRuntime jsRuntime,
+            string pathFromWwwRoot)
+        {
+            var module = await jsRuntime.InvokeAsync<IJSObjectReference>(
+                "import",
+                Path.Combine($"./_content/Blazy/", pathFromWwwRoot)
+            );
+            return module;
+        }
+    }
+}
