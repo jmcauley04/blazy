997db8b25ee76c0bf9a0e5535ead4da6bbf78f01
Michael Cauley <MCauley@nms.org>
2022-01-04 17:40:13 -0500

    Added base client for easy reuse

---
+++ Blazy/Abstractions/BaseHttpClient.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+
+namespace Blazy.Abstractions
+{
+    public abstract class BaseHttpClient<THttpClient>
+        where THttpClient : HttpClient
+    {
+        protected THttpClient _client;
+
+        protected abstract string _baseEndpoint { get; }
+
+        public BaseHttpClient(THttpClient client)
+        {
+            _client = client;
+        }
+
+        #region get
+
+        /// <summary>
+        /// Use to hit an endpoint without getting items back
+        /// </summary>
+        public async Task BaseGetAsync(string endpoint, CancellationToken ct = default)
+        {
+            var response = await _client.GetAsync(endpoint, ct);
+
+            if (response.IsSuccessStatusCode)
+                return;
+
+            throw new Exception($"{response.StatusCode}: {await response.Content.ReadAsStringAsync()}");
+        }
+
+        /// <summary>
+        /// Use to get item(s) from an api endpoint
+        /// </summary>
+        public async Task<TItem> BaseGetAsync<TItem>(string endpoint, CancellationToken ct = default)
+        {
+            var response = await _client.GetAsync(endpoint, ct);
+
+            if (response.IsSuccessStatusCode)
+                return await response.Content.ReadFromJsonAsync<TItem>();
+
+            throw new Exception($"{response.StatusCode}: {await response.Content.ReadAsStringAsync()}");
+        }
+
+        #endregion
+
+        #region put
+
+        /// <summary>
+        /// Use to updating an existing item from an api endpoint
+        /// </summary>
+        public async Task BasePutAsync<TItem>(string endpoint, TItem item, CancellationToken ct = default)
+        {
+            var response = await _client.PutAsJsonAsync(endpoint, item, ct);
+
+            if (response.IsSuccessStatusCode)
+                return;
+
+            throw new Exception($"{response.StatusCode}: {await response.Content.ReadAsStringAsync()}");
+        }
+
+        #endregion
+
+        #region post
+
+        /// <summary>
+        /// Use to create a new item and getting an item returned from an api endpoint
+        /// </summary>
+        public async Task<TItem> BasePostAsync<TItem>(string endpoint, TItem item, CancellationToken ct = default)
+        {
+            var response = await _client.PostAsJsonAsync(endpoint, item, ct);
+
+            if (response.IsSuccessStatusCode)
+                return await response.Content.ReadFromJsonAsync<TItem>();
+
+            throw new Exception($"{response.StatusCode}: {await response.Content.ReadAsStringAsync()}");
+        }
+
+        #endregion
+
+        #region delete
+
+        /// <summary>
+        /// Use to delete an item from an api endpoint
+        /// </summary>
+        public async Task BaseDeleteAsync(string endpoint, CancellationToken ct = default)
+        {
+            var response = await _client.DeleteAsync(endpoint, ct);
+
+            if (response.IsSuccessStatusCode)
+                return;
+
+            throw new Exception($"{response.StatusCode}: {await response.Content.ReadAsStringAsync()}");
+        }
+
+        #endregion
+    }
+}
