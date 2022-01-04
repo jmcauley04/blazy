c46e1a67b67d660c36ba08d26145a3d92aaa9e6e
Michael Cauley <jmcauley04@gmail.com>
2022-01-04 07:21:01 -0500

    Implemented Blazy Height Transition and applied to accordion and all other height transitions.

--- Blazy/Extensions/JsRuntimeExtensions.cs
+++ Blazy/Extensions/JsRuntimeExtensions.cs
@@ -7,13 +7,25 @@ namespace Blazy.Extensions
     {
         internal static async Task<IJSObjectReference> Import(
             this IJSRuntime jsRuntime,
-            string pathFromWwwRoot)
+            string path)
         {
             var module = await jsRuntime.InvokeAsync<IJSObjectReference>(
                 "import",
-                Path.Combine($"./_content/Blazy/", pathFromWwwRoot)
+                Path.Combine($"./_content/Blazy/", path)
             );
             return module;
+        }
+
+        internal static async Task<IJSObjectReference> Import<T>(
+            this IJSRuntime jsRuntime,
+            T instance)
+        {
+            var path = instance!.GetType().FullName!.Replace('.', '/');
+            var module = await jsRuntime.InvokeAsync<IJSObjectReference>(
+                "import",
+                Path.Combine($"./_content/", $"{path}.razor.js")
+            );
+            return module;            
         }
     }
 }
