5e5ba147a7e42936a97f7a477d5e90bb0b6b3c9b
jmcauley04 <jmcauley04@users.noreply.github.com>
2022-01-04 12:23:56 +0000

    Deploying to gh-pages from  @ 48b2bacb8d007c8fe94951fa9277d445f2d052e8 🚀

---
+++ _content/Blazy/Effects/BlazyHeightTransition.razor.js
@@ -0,0 +1,4 @@
+﻿
+export function getScrollHeight(element) {
+    return element.scrollHeight;
+}
--- _content/Blazy/blazyJsInterop.js
+++ _content/Blazy/blazyJsInterop.js
@@ -2,3 +2,7 @@
 export function getInnerHtml(element) {
     return element.innerHTML;
 }
+
+export function showBlazyPrompt(message) {
+    return prompt(message, 'Type anything here');
+}
