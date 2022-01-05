66b1df099ae865f4ead7a1e421fe275ae2d09708
Michael Cauley <jmcauley04@gmail.com>
2022-01-05 05:50:51 -0500

    Progress bar completed

---
+++ Blazy/Extensions/IntExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blazy.Extensions
+{
+    public static class IntExtensions
+    {
+        public static int Clamp(this int number, int min, int max) => Math.Max(min, Math.Min(number, max));
+    }
+}
