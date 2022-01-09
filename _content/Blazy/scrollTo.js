2fb99515b790f13934ea49359472c066d62eb457
jmcauley04 <57710228+jmcauley04@users.noreply.github.com>
2022-01-09 21:50:28 +0000

    Deploying to gh-pages from  @ 106745c18820a2f22a92e34034bdccf5e33a7e6b 🚀

---
+++ _content/Blazy/scrollTo.js
@@ -0,0 +1,7 @@
+﻿
+export function scrollToY(element) {
+    let main = document.getElementsByTagName('main');
+    let position = element.getBoundingClientRect();
+    let y = position.top;
+    return window.scrollTo(0, y);
+}
