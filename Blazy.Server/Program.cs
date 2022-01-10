202c2d581baf802a1c6f2777a4cdcd4949d79a09
Michael Cauley <jmcauley04@gmail.com>
2022-01-10 06:52:10 -0500

    Closes #12

--- Blazy.Server/Program.cs
+++ Blazy.Server/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(@"https://localhost:7097/") });
 
 var app = builder.Build();
 
